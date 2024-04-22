50bb97f3dbd2e3e5ddee7bf8e13279faf0d8658f
PerchunPak <git@perchun.it>
2024-04-22 15:12:57 +0200

    Remove visited links and use already storred data

--- src/db.py
+++ src/db.py
@@ -13,7 +13,6 @@ class Database:
     def __init__(self) -> None:
         self._words: dict[str, set[str]] = {"__total": set()}
         self._links: dict[str, set[str]] = {"__total": set()}
-        self._visited_links: set[str] = set()
         self._read()
 
     def _read(self) -> None:
@@ -24,14 +23,12 @@ class Database:
             as_dict = json.load(f)
             self._words = self._unsanitize(as_dict["words"])
             self._links = self._unsanitize(as_dict["links"])
-            self._visited_links = set(as_dict["visited_links"])
 
     def _write(self) -> None:
         with DB_FILE.open("w") as f:
             as_dict = {
                 "words": self._sanitize(self._words),
                 "links": self._sanitize(self._links),
-                "visited_links": list(self._visited_links),
             }
             json.dump(as_dict, f)
 
@@ -56,10 +53,9 @@ class Database:
 
             all_links = self._links["__total"].copy()
             for link in all_links:
-                if link not in self._visited_links:
+                if link not in self._words:
                     found_unvisited_link = True
                     yield link
-                    self._visited_links.add(link)
 
     def add_words(self, site: str, words: set[str]) -> None:
         self._words[site] = words
