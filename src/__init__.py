ce2874670fc1a0b42d96e55fdf15b2cd38c28636
PerchunPak <git@perchun.it>
2024-04-22 14:15:38 +0200

    Add first prototype

--- src/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Main package for ``word-crawler``."""
--- src/__main__.py
+++ src/__main__.py
@@ -1,19 +1,38 @@
 import asyncio
+import dataclasses
+import sys
 
+import aiohttp
+from bs4 import BeautifulSoup
 from loguru import logger
 
-from src import utils
-from src.config import Config
+
+@dataclasses.dataclass
+class SiteResult:
+    words: set[str]
+    links: set[str]
+
+
+async def get_words_and_links(session: aiohttp.ClientSession, site: str) -> SiteResult:
+    async with session.get(site) as answer:
+        soup = BeautifulSoup(await answer.text(), "html.parser")
+        print(soup.text.split())
+        for link in soup.find_all("a"):
+            print(link)
+        return SiteResult(
+            links=
+        )
 
 
 async def main() -> None:
-    utils.setup_logging()
-    logger.info("Hello World!")
-
-    Config()
-    utils.start_sentry()
-    await utils.start_apykuma()
-    # start app here
+    site = sys.argv[1]
+    logger.info(f"Parsing {site!r}...")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(site) as answer:
+            soup = BeautifulSoup(await answer.text(), "html.parser")
+            print(soup.text.split())
+            for link in soup.find_all("a"):
+                print(link)
 
 
 if __name__ == "__main__":
